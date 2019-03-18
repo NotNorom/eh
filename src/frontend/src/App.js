27747c0a13e20abe0f798905c3a5cbc0e73fb949
Andre Julius <noromoron@gmail.com>
2019-03-18 07:25:39 +0100

    Add currentlyPlaying field to state and forward it to the video player

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-// import config from "./config.js";
 import VideoPlayer from "./VideoPlayer/VideoPlayer";
 import Queue from "./Queue/Queue";
 import "./App.css";
+
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            currentlyPlaying: "b06pKMxF6h8",
             queue: [
                 "lBsq4DC6Jv4",
                 "WZFVeSZrM_I",
             ]
         }
     }
+
     render() {
         return (
             <React.Fragment>
                 <header>
-                    <VideoPlayer />
+                    <VideoPlayer videoId={this.state.currentlyPlaying}/>
                 </header>
                 <main>
                     <Queue queue={this.state.queue}/>
