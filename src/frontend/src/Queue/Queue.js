0febfdd2525455514ff029bee15c98191421ae5f
Andre Julius <noromoron@gmail.com>
2019-03-20 02:12:10 +0100

    Relay onPlay, onMoveUp, onMoveDown, onDelete functions to queue entry.
    Change key generation for queue entry. This should reduce requests to the youtube data api.

--- src/frontend/src/Queue/Queue.js
+++ src/frontend/src/Queue/Queue.js
@@ -4,8 +4,21 @@ import QueueEntry from "./QueueEntry";
 
 class Queue extends Component {
     render() {
-        const queueEntries = this.props.queue.map(entry => {
-            return <QueueEntry key={entry} entry={entry} onPlay={this.props.onPlay.bind(null, entry)}/>
+        const queueEntries = this.props.queue.map((entry, index, array) => {
+            // start searching from the next position
+            var indexOfDuplicate = array.indexOf(entry, index+1);
+            return <QueueEntry
+                // if no duplicate is found, just use the video id.
+                // if a duplicate is found, append the index of the duplicate.
+                // the duplicate check is made to not spam youtubes data api.
+                key={`${entry}${indexOfDuplicate <= 0 ? "" : indexOfDuplicate}`}
+                entry={entry}
+                index={index}
+                onPlay={this.props.onPlay.bind(null, entry, index)}
+                onMoveUp={this.props.onMoveUp.bind(null, entry, index)}
+                onMoveDown={this.props.onMoveDown.bind(null, entry, index)}
+                onDelete={this.props.onDelete.bind(null, entry, index)}
+            />
         });
         return (
             <div className={style.queueContainer}>
